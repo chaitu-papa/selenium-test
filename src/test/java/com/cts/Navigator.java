5bccd41e10360eb2287b5af57bada7e6fc1c2ea0
Krishna Papa <chaitu.papa@gmail.com>
2020-02-07 11:31:28 +0000

    test code

--- src/test/java/com/cts/Navigator.java
+++ src/test/java/com/cts/Navigator.java
@@ -1,7 +1,7 @@
 package Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
 
